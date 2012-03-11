d2a264bb8ab4bbc6f310949e34db8b667bd7997b
Marlon Patrick <marlonpatrick10@gmail.com>
2012-03-11 04:01:24 -0300

    Configurando blog

--- plugins/date.rb
+++ plugins/date.rb
@@ -1,3 +1,18 @@
+Date::MONTHNAMES = [nil] + %w(Janeiro Fevereiro Marco Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro)  
+Date::DAYNAMES = %w(Domingo Segunda-Feira Terca-Feira Quarta-Feira Quinta-Feira Sexta-Feira Sabado)  
+Date::ABBR_MONTHNAMES = [nil] + %w(Jan Fev Mar Abr Mai Jun Jul Aug Sep Out Nov Dez)  
+Date::ABBR_DAYNAMES = %w(Dom Seg Ter Qua Qui Sex Sab)
+class Time  
+  alias :strftime_nolocale :strftime  
+  def strftime(format)  
+    format = format.dup  
+    format.gsub!(/%a/, Date::ABBR_DAYNAMES[self.wday])  
+    format.gsub!(/%A/, Date::DAYNAMES[self.wday])  
+    format.gsub!(/%b/, Date::ABBR_MONTHNAMES[self.mon])  
+    format.gsub!(/%B/, Date::MONTHNAMES[self.mon])  
+    self.strftime_nolocale(format)  
+  end  
+end
 module Octopress
   module Date
 
